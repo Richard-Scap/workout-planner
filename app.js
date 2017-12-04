c6b22e756fd77ad71285034f0f7adf9a177b01ad
Richard-Scap <scap222@gmail.com>
2017-12-03 23:51:26 -0600

    make heroku work

--- app.js
+++ app.js
@@ -48,4 +48,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env))
+app.listen(process.env.PORT || 3000, () => console.log("Express server listening on port %d in %s mode", app.address().port, config))
